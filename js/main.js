5a5aaa66d0c8239822a89995a14174dee0882ab7
OskarKlo <oskark@thinkglobalschool.com>
2017-06-10 13:58:11 -0700

    Merged signIn.js with main.js, functional auth (firebase) and working on the log out callback on main nav

--- js/main.js
+++ js/main.js
@@ -1,35 +1,65 @@
-// Sample Working Button by ID
-var handleClick = function (event) {
-    alert("it was clicked");
+    
+    // const logIn = document.getElementById('signIn');
+
+    // logIn.addEventListener('click', e => {
+    // window.location.replace("signIn.html");
+    // });
+
+//Firebase Auth
+    // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB34kc_4jvEF3R7IeeGZ_rT_g5Kg0T7aWk",
+    authDomain: "studio-8a79b.firebaseapp.com",
+    databaseURL: "https://studio-8a79b.firebaseio.com",
+    projectId: "studio-8a79b",
+    storageBucket: "studio-8a79b.appspot.com",
+    messagingSenderId: "1017262570857"
 };
-var button = document.getElementById('trial-button');
-button.addEventListener('click', handleClick);
+  firebase.initializeApp(config);
 
-// var req = new XMLHttpRequest();
-// req.onload = function (event) { . . . };
-// req.open('get', 'some-file.txt', true);
-// req.send();
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignUp = document.getElementById('btnSignUp');
+    const btnLogout = document.getElementById('btnLogout');
+    const logOut = document.getElementById('logOut');
 
-$('.trial').css('color', 'white');
+    //Login Event
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        //Sign In
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
 
-$.get('/data.json', function (data) {
-    console.log("IT worked");
-}).fail(function () {
-    console.log("IT failed");
-});
+    //SignUp event
+    btnSignUp.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        //Sign In
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
 
-$.post('index.html', { username: 'tom' }, function (data) {
-    console.log(data);
-}).fail(function () {
-    console.log('another fail');
-});
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+    });
 
-// When Window Loads
-// var alertAQuestion = function (event) {
-//     console.log("Window loaded!");
-// };
+    // Add a realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+            window.location.replace("index.html");
+            btnLogout.classList.remove('hide');
+            logOut.classList.remove('hide');
+        } else {
+            console.log('not logged in');
+            btnLogout.classList.add('hide');
+            logOut.classList.add('hide');
+        }
+    });
 
-// $(window).ready(alertAQuestion);
-
-// $(alertAQuestion);
-
+    //End of Auth
